5cbc1e865cd1833b57a172611c473170a65499df
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2026-08-11 14:56:36 +0400

    feat: удалена реализация web

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/service/HubEventService.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.telemetry.service;
-
-import ru.yandex.practicum.telemetry.dto.event.hub.HubEvent;
-
-public interface HubEventService {
-    void send(HubEvent event);
-}
