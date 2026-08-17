9b46d9e92c73f3f74e6b99e023d6301c6427d9f6
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2026-08-17 11:17:25 +0400

    refactor: удален неиспользуемый импорт

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/KafkaConfig.java
@@ -8,7 +8,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import ru.yandex.practicum.telemetry.kafka.EventSerializer;
 
 import java.util.Properties;
